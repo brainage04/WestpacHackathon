2376971d4d3d27952529893c5f73999340182b49
brainage04 <thomasmcmah21@gmail.com>
2024-07-03 15:04:20 +1000

    python script to prepare kaggle archive

---
+++ archive.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+DATASET_ROOT = "16000_pcm_speeches"
+
+AUDIO_SUBFOLDER = "audio"
+NOISE_SUBFOLDER = "noise"
+
+DATASET_AUDIO_PATH = os.path.join(DATASET_ROOT, AUDIO_SUBFOLDER)
+DATASET_NOISE_PATH = os.path.join(DATASET_ROOT, NOISE_SUBFOLDER)
+
+for folder in os.listdir(DATASET_ROOT):
+    if os.path.isdir(os.path.join(DATASET_ROOT, folder)):
+        if folder in [AUDIO_SUBFOLDER, NOISE_SUBFOLDER]:
+            # If folder is `audio` or `noise`, do nothing
+            continue
+        elif folder in ["other", "_background_noise_"]:
+            # If folder is one of the folders that contains noise samples,
+            # move it to the `noise` folder
+            shutil.move(
+                os.path.join(DATASET_ROOT, folder),
+                os.path.join(DATASET_NOISE_PATH, folder),
+            )
+        else:
+            # Otherwise, it should be a speaker folder, then move it to
+            # `audio` folder
+            shutil.move(
+                os.path.join(DATASET_ROOT, folder),
+                os.path.join(DATASET_AUDIO_PATH, folder),
+            )
