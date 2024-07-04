99b9291bd91eda04b48ba71856c323dbc5557e6f
brainage04 <thomasmcmah21@gmail.com>
2024-07-04 10:35:14 +1000

    archive script works now

--- archive_README.py
+++ archive_README.py
@@ -1,95 +1,49 @@
-## RUN THIS BEFORE RUNNING BACK_END.PY
-
 import os
-os.environ["KERAS_BACKEND"] = "tensorflow"
-import tensorflow as tf
-from pathlib import Path
+import wave
 
-DATASET_ROOT = "16000_pcm_speeches"
+input_path = os.path.join(os.getcwd(), "16000_pcm_speeches", "audio")
+output_path = os.path.join(os.getcwd(), "16000_pcm_speeches_MODIFIED", "audio")
 
-# The folders in which we will put the audio samples and the noise samples
-AUDIO_SUBFOLDER = "audio"
-NOISE_SUBFOLDER = "noise"
+input_paths = [
+    os.path.join(input_path, "Benjamin_Netanyau"),
+    os.path.join(input_path, "Jens_Stoltenberg"),
+    os.path.join(input_path, "Julia_Gillard"),
+    os.path.join(input_path, "Magaret_Tarcher"),
+    os.path.join(input_path, "Nelson_Mandela"),
+]
+output_paths = [
+    os.path.join(output_path, "Benjamin_Netanyau"),
+    os.path.join(output_path, "Jens_Stoltenberg"),
+    os.path.join(output_path, "Julia_Gillard"),
+    os.path.join(output_path, "Magaret_Tarcher"),
+    os.path.join(output_path, "Nelson_Mandela"),
+]
 
-DATASET_AUDIO_PATH = os.path.join(DATASET_ROOT, AUDIO_SUBFOLDER)
-DATASET_NOISE_PATH = os.path.join(DATASET_ROOT, NOISE_SUBFOLDER)
+for i in range(len(input_paths)):
+    print(f"Combining files in {input_paths[i]}...")
 
-# Percentage of samples to use for validation
-VALID_SPLIT = 0.1
-
-# Seed to use when shuffling the dataset and the noise
-SHUFFLE_SEED = 43
-
-# The sampling rate to use.
-# This is the one used in all the audio samples.
-# We will resample all the noise to this sampling rate.
-# This will also be the output size of the audio wave samples
-# (since all samples are of 1 second long)
-SAMPLING_RATE = 16000
-
-# The factor to multiply the noise with according to:
-#   noisy_sample = sample + noise * prop * scale
-#      where prop = sample_amplitude / noise_amplitude
-SCALE = 0.5
-
-BATCH_SIZE = 128
-EPOCHS = 1
-
-# Get the list of all noise files
-noise_paths = []
-for subdir in os.listdir(DATASET_NOISE_PATH):
-    subdir_path = Path(DATASET_NOISE_PATH) / subdir
-    if os.path.isdir(subdir_path):
-        noise_paths += [
-            os.path.join(subdir_path, filepath)
-            for filepath in os.listdir(subdir_path)
-            if filepath.endswith(".wav")
+    for j in range(300):
+        infiles = [
+            os.path.join(input_paths[i], f"{j * 5 + 0}.wav"),
+            os.path.join(input_paths[i], f"{j * 5 + 1}.wav"),
+            os.path.join(input_paths[i], f"{j * 5 + 2}.wav"),
+            os.path.join(input_paths[i], f"{j * 5 + 3}.wav"),
+            os.path.join(input_paths[i], f"{j * 5 + 4}.wav"),
         ]
-if not noise_paths:
-    raise RuntimeError(f"Could not find any files at {DATASET_NOISE_PATH}")
-print(
-    "Found {} files belonging to {} directories".format(
-        len(noise_paths), len(os.listdir(DATASET_NOISE_PATH))
-    )
-)
+        outfile = os.path.join(output_paths[i], f"{j}.wav")
 
-command = (
-    "for dir in `ls -1 " + DATASET_NOISE_PATH + "`; do "
-    "for file in `ls -1 " + DATASET_NOISE_PATH + "/$dir/*.wav`; do "
-    "sample_rate=`ffprobe -hide_banner -loglevel panic -show_streams "
-    "$file | grep sample_rate | cut -f2 -d=`; "
-    "if [ $sample_rate -ne 16000 ]; then "
-    "ffmpeg -hide_banner -loglevel panic -y "
-    "-i $file -ar 16000 temp.wav; "
-    "mv temp.wav $file; "
-    "fi; done; done"
-)
-os.system(command)
+        data=[]
+        for infile in infiles:
+            w = wave.open(infile, 'rb')
+            data.append( [w.getparams(), w.readframes(w.getnframes())] )
+            w.close()
+            
+        output = wave.open(outfile, 'wb')
+        output.setparams(data[0][0])
+        for k in range(len(data)):
+            output.writeframes(data[k][1])
+        output.close()
 
-# Split noise into chunks of 16,000 steps each
-def load_noise_sample(path):
-    sample, sampling_rate = tf.audio.decode_wav(
-        tf.io.read_file(path), desired_channels=1
-    )
-    if sampling_rate == SAMPLING_RATE:
-        # Number of slices of 16000 each that can be generated from the noise sample
-        slices = int(sample.shape[0] / SAMPLING_RATE)
-        sample = tf.split(sample[: slices * SAMPLING_RATE], slices)
-        return sample
-    else:
-        print("Sampling rate for {} is incorrect. Ignoring it".format(path))
-        return None
+        print(f"File {outfile} created.")
 
-
-noises = []
-for path in noise_paths:
-    sample = load_noise_sample(path)
-    if sample:
-        noises.extend(sample)
-noises = tf.stack(noises)
-
-print(
-    "{} noise files were split into {} noise samples where each is {} sec. long".format(
-        len(noise_paths), noises.shape[0], noises.shape[1] // SAMPLING_RATE
-    )
-)
+    print("Files combined successfully.")
