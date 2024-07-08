fd07ac0d46f51d3529c512675f1e5e3d47edbc31
brainage04 <thomasmcmah21@gmail.com>
2024-07-08 17:32:17 +1000

    more changes to test file

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ from main_copy import path_to_fft
 
 model = keras.models.load_model(os.path.join(os.getcwd(), "model.keras"))
 
-print(model.summary())
+model.summary()
+model.summary(expand_nested=True)
 
 first_sample = path_to_fft(os.path.join(os.getcwd(), 'Sounds', 'Thomas', 'thomas_original.wav'))
 second_sample = path_to_fft(os.path.join(os.getcwd(), 'Sounds', 'Thomas', 'thomas_original.wav'))
