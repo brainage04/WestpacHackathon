cac531f74b234d98215d253a40073d9348c85c91
brainage04 <thomasmcmah21@gmail.com>
2024-07-08 17:25:11 +1000

    testing file

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import os
 import keras
+import numpy as np
+import tensorflow as tf
 from main_copy import path_to_fft
 
-model = keras.models.load_model(os.path.join(os.getcwd(), "model.keras"), compile=False)
+model = keras.models.load_model(os.path.join(os.getcwd(), "model.keras"))
+
+print(model.summary())
 
 first_sample = path_to_fft(os.path.join(os.getcwd(), 'Sounds', 'Thomas', 'thomas_original.wav'))
 second_sample = path_to_fft(os.path.join(os.getcwd(), 'Sounds', 'Thomas', 'thomas_original.wav'))
+
+print("First: ", first_sample.shape)
+print("Second: ", second_sample.shape)
 
 prediction = model.predict([first_sample, second_sample])
 print(prediction)
